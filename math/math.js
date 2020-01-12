c33b9ffa96ce2bbd50928ce57c2a536a8129b316
Kenny <yu.kenny.k@gmail.com>
2020-01-11 20:23:43 -0800

    add isCharDigit and Yuros

--- math/math.js
+++ math/math.js
@@ -1,3 +1,5 @@
+// note the magic sequence: 24816. 2*8 = 4*4 = 16
+// Yuros, Yu's rule of square: For a square s and root r, there is a u under r, an o over r such that u*o=s
 function sqrt(x){
    for (let guess = (1.0 + x)/2, root = 0; ; guess = root){
      root = (x/guess + guess)/2; //https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method
@@ -26,3 +28,11 @@ const getPrimes = (n = 0) => {
     return primes;
 }
 
+//test if a char is a digit
+const isCharDigit = n => n < 10;  // a digit char is converted to a number
+const isDigit = n => +n == n;
+const isDigit = n => !isNaN(n);
+const isCharDigit = n => !!++n;
+function isCharDigit(n){
+  return !!n.trim() && n > -1;
+}
