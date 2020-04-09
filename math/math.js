156414c6677b6e6e1e5c949510b82c73883f2719
Kenny <yu.kenny.k@gmail.com>
2020-04-09 20:48:44 +0000

    symplify pythagorean triple

--- math/math.js
+++ math/math.js
@@ -1,31 +1,33 @@
 // note the magic sequence: 24816. 2*8 = 4*4 = 16
 // Yuros, Yu's rule of square: For a square s and root r, there is a u under r, an o over r such that u*o=s
-function sqrt(x){
-   for (let guess = (1.0 + x)/2, root = 0; ; guess = root){
-     root = (x/guess + guess)/2; //https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method
-     console.log(`root = ${root}; guess = ${guess}`);
-     if(Math.abs(root - guess) < 0.000001) return root;
-   }
+//https://www.mathsisfun.com/square-root.html
+//https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method
+function sqrt(x) {
+  for (let guess = (1.0 + x) / 2, root = 0; ; guess = root) {
+    root = (x / guess + guess) / 2; 
+    console.log(`root = ${root}; guess = ${guess}`);
+    if (Math.abs(root - guess) < 0.000001) return root;
+  }
 };
 
 function isPrime(value) {
-    for (var i = 2; i < Math.sqrt(value); i++) {
-        if (value % i === 0) {
-            return false;
-        }
+  for (var i = 2; i < Math.sqrt(value); i++) {
+    if (value % i === 0) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
 const getPrimes = (n = 0) => {
-    const primes = n < 2 ? [] : [2];
+  const primes = n < 2 ? [] : [2];
 
-    for (let i = 3; i <= n; i += 2) {
-        if (primes.every(x => i % x))
-            primes.push(i);
-    }
+  for (let i = 3; i <= n; i += 2) {
+    if (primes.every(x => i % x))
+      primes.push(i);
+  }
 
-    return primes;
+  return primes;
 }
 
 //test if a char is a digit
@@ -33,6 +35,6 @@ const isCharDigit = n => n < 10;  // a digit char is converted to a number
 const isDigit = n => +n == n;
 const isDigit = n => !isNaN(n);
 const isCharDigit = n => !!++n;
-function isCharDigit(n){
+function isCharDigit(n) {
   return !!n.trim() && n > -1;
 }
