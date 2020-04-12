a4ca2de8af142dc8424ed93f5d04cd499963101b
Kenny <yu.kenny.k@gmail.com>
2020-04-12 23:24:52 +0000

    update fibo

--- math/fibo.js
+++ math/fibo.js
@@ -1,7 +1,7 @@
-fibo = {
-  let sequence = [1, 2];
+const fibo = {
+  sequence: [1, 2];
 
-  next: () => sequence[sequence.length - 2] + sequence[sequence.length - 1]
+  next: () => sequence[sequence.length - 2] + sequence[sequence.length - 1];
 
   grow: () => sequence.push(next());
 
