ccb3b1b2de3752dde2b6476b4ee6773bb80a2830
Kenny <yu.kenny.k@gmail.com>
2020-06-14 19:45:07 +0000

    finish binary calculator

--- math/algos.js
+++ math/algos.js
@@ -33,3 +33,41 @@ const selectionSort = function (list) {
   }
   return list;
 }
+
+const modeF = arr => { if (arr.filter((x, index) => arr.indexOf(x) == index).length == arr.length) return arr; else return modeF(arr.sort((x, index) => x - index).map((x, index) => arr.indexOf(x) != index ? x : null).filter(x => x != null)) };
+
+
+// https://stackoverflow.com/questions/52898456/simplest-way-of-finding-mode-in-javascript
+const modeS = a => {
+  a.sort((x, y) => x - y);
+
+  var bestStreak = 1;
+  var bestElem = a[0];
+  var currentStreak = 1;
+  var currentElem = a[0];
+
+  for (let i = 1; i < a.length; i++) {
+    if (a[i - 1] !== a[i]) {
+      if (currentStreak > bestStreak) {
+        bestStreak = currentStreak;
+        bestElem = currentElem;
+      }
+      currentStreak = 0;
+      currentElem = a[i];
+    }
+    currentStreak++;
+  }
+  return currentStreak > bestStreak ? currentElem : bestElem;
+};
+
+const modeO = a =>
+  Object.values(
+    a.reduce((count, e) => {
+      if (!(e in count)) {
+        count[e] = [0, e];
+      }
+
+      count[e][0]++;
+      return count;
+    }, {})
+  ).reduce((a, v) => v[0] < a[0] ? a : v, [0, null])[1];
