f38dcf184b23875f241dbe08a485a9ec53e279a4
Kenny <yu.kenny.k@gmail.com>
2020-08-05 02:29:57 +0000

    triangle class

--- math/Triangle.js
+++ math/Triangle.js
@@ -1,5 +1,20 @@
+class Point{
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+  }
+}
+/*
+https://mathworld.wolfram.com/Triangle.html#:~:text=Every%20triangle%20has%20three%20sides,triangles%20are%20convex%20and%20bicentric.
+
+This captures knowledge on triangles.
+
+For ease of drawing an arbitrary triangle, we lay it on its long side. That is, the long side is its base.
+
+*/
 class Triangle {
   constructor(a, b, c) {
+    // The three parameters, a,b,and c, are the length of three sides. They may come in any order.
     let sides = [a, b, c].sort();
     this.longSide = sides[2];
     this.midSide = sides[1];
@@ -27,5 +42,17 @@ class Triangle {
     let flatFar = { x: this.longSide, y: 0 };
     let apex = { x: Math.sqrt((this.shortSide) ** 2 - (this.getHeight()) ** 2), y: this.getHeight() };
     return [origin, flatFar, apex];
+  }
+
+  getApexPoint(){
+    return new Point(Math.sqrt((this.shortSide) ** 2 - (this.getHeight()) ** 2), this.getHeight());
+  }
+
+  getLeftBasePoint(){
+    return new Point(0, 0);
+  }
+
+  getRightBasePoint(){
+    return new Point(this.longSide, 0);
   }
 }
